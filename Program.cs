fb786a115c815ebfb80fe9ecd2987e0e7b2700d3
S1mPLe756 <145194790+S1mPLe756@users.noreply.github.com>
2023-09-19 21:37:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//проверить является ли число степенью 3
+while (true)
+{
+    int n, i;
+    n = int.Parse(Console.ReadLine());
+    i = 1;
+    while (i < n)
+    {
+        i = i * 3;
+    }
+    if (n == i)
+    {
+        Console.WriteLine("True");
+    }
+    else { Console.WriteLine("False"); }
+}
